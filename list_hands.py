752d3d9be4e50d6dbffa2cb87fd783f910f80fb2
akai <guanyankai@qike366.com>
2022-08-05 11:50:42 +0800

    对列表中的字典对象去重

---
+++ list_hands.py
@@ -0,0 +1,7 @@
+from functools import reduce
+
+data_list = [{'project_id': '14', 'project_name': '项目名称'}, {'project_id': '12', 'project_name': '第er47735三共长疗程'},
+             {'project_id': '16', 'project_name': '阿斯利康'}, {'project_id': '12', 'project_name': '第er47735三共长疗程'}]
+data_list.insert(0, [])
+res = reduce(lambda x, y: x if y in x else x + [y], data_list)
+print(res)
